6a9fa2f5894dd5bd3e86d4b951f6e3e2f25d2b23
Vitaliy-Yakovlev <vitaliy-yakovlev@ukr.net>
2021-07-29 20:28:21 +0300

    Добавил Пролистывание изображений галереи

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -13,3 +13,48 @@
 * следующий div после первого, должен быть шире и выше предыдущего на 10px Создай
 * функцию destroyBoxes(), которая очищает div#boxes.
 */
+
+const refs = {
+  input: document.querySelector('input'),
+  renderBtn: document.querySelector('[data-action="render"]'),
+  destroyBtn: document.querySelector('[data-action="destroy"]'),
+  boxes: document.querySelector('#boxes'),
+};
+
+let arrayDivs = [];
+
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+refs.renderBtn.addEventListener('click', renderNumberBoxes);
+
+function createDivs(amount) {
+  arrayDivs = [];
+  refs.boxes.innerHTML = '';
+
+  let size = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    size += 10;
+
+    const createDiv = document.createElement('div');
+    createDiv.style.width = `${size}px`;
+    createDiv.style.height = `${size}px`;
+    createDiv.style.backgroundColor = `#${Math.random()
+      .toString(16)
+      .substr(-6)}`;
+    arrayDivs.push(createDiv);
+  }
+
+  refs.boxes.append(...arrayDivs);
+}
+
+function destroyBoxes() {
+  arrayDivs = [];
+  refs.boxes.innerHTML = '';
+  refs.input.value = '';
+}
+
+function renderNumberBoxes() {
+  const value = Number(refs.input.value);
+
+  createDivs(value);
+}
